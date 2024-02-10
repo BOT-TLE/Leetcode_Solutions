75a13fd68f256846e119cf041619b5d46af822b3
Chirayu Baliyan <129618602+BOT-TLE@users.noreply.github.com>
2024-02-10 10:21:24 +0530

    Time: 5 ms (18.43%), Space: 10.1 MB (36.73%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int val = digits[digits.size() - 1];
+        if(val < 9)
+        {
+            digits[digits.size() - 1]++;
+            return digits;
+        }
+        else{
+            vector<int> ans;
+            bool gotIt = false;
+            for(int i = digits.size() - 1 ; i >= 0 ; --i)
+            {
+                if(digits[i] == 9 and !gotIt)
+                {
+                    ans.emplace_back(0);
+                }
+                if(digits[i] != 9 and !gotIt)
+                {
+                    digits[i]++;
+                    gotIt = true;
+                }
+                if(gotIt)
+                    ans.emplace_back(digits[i]);
+            }
+            int sum = accumulate(ans.begin(),ans.end(),0);
+            if(sum == 0)
+                ans.emplace_back(1);
+            reverse(ans.begin(),ans.end());
+            return ans;
+        }
+    }
+};
